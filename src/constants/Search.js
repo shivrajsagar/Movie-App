58e4ff8e118ec41a7c838900629b1934078bd5d0
Suraj Prasad <surajvishwakarma830@gmail.com>
2020-12-17 19:00:21 +0530

    comit

--- src/constants/Search.js
+++ src/constants/Search.js
@@ -3,7 +3,7 @@ const Search = () => {
     <div className="container p-32 items-center justify-center text-center from-blue-600 via-teal-500 to-purple-500 bg-gradient-to-r w-full h-64 block ">
       <h1>Search</h1>
       <form>
-        <input type="search"  className="" />
+        <input type="search"  className="h-10" />
       </form>
     </div>
   );
